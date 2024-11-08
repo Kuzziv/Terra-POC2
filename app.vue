0e1a7834de744036cb282f0fd3b4e7f7625b2999
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2024-11-08 21:12:19 +0100

    app.vue update

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
+<script setup lang="ts">
+  // Your TypeScript code here
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
-
-<script setup lang="ts">
-  // Your TypeScript code here
-</script>
